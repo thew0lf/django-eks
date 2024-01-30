6c07c90b0c160d7512d3f98b8d0b9847873aaf6d
thew0lf <bill@tandgconsulting.com>
2024-01-30 13:56:46 -0500

    Added EKS ECR

--- infra/eks.tf
+++ infra/eks.tf
@@ -12,7 +12,7 @@ module "eks"{
   vpc_id = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
   cluster_endpoint_public_access = true
-  cluster_endpoint_public_access_cidrs = ["${var.my_ip}/32"]
+  cluster_endpoint_public_access_ciders = ["${var.my_ip}/32"]
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
